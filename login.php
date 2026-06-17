bb1b55e187e152faff156df075252647cc23c5c5
aldo-2903 <eltukis772@gmail.com>
2026-06-16 23:52:33 -0600

    Add files via upload

---
+++ login.php
@@ -0,0 +1,23 @@
+<?php
+session_start();
+include("conexion.php");
+
+$correo = $_POST['correo'] ?? '';
+$password = $_POST['password'] ?? '';
+
+$sql = "SELECT * FROM usuarios WHERE correo = ?";
+$stmt = $conexion->prepare($sql);
+$stmt->execute([$correo]);
+
+$user = $stmt->fetch(PDO::FETCH_ASSOC);
+
+if ($user && password_verify($password, $user['password'])) {
+
+    $_SESSION['usuario_id'] = $user['id'];
+    $_SESSION['usuario_nombre'] = $user['nombre'];
+
+    echo "ok";
+} else {
+    echo "error";
+}
+?>
