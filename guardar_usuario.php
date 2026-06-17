e4937d5d5f1f3339aae030e67d155083454769c6
aldo-2903 <eltukis772@gmail.com>
2026-06-16 23:01:23 -0600

    Update guardar_usuario.php

--- guardar_usuario.php
+++ guardar_usuario.php
@@ -1,22 +1,20 @@
 <?php
-
 include("conexion.php");
 
-$nombre = $_POST['nombre'];
-$correo = $_POST['correo'];
-$telefono = $_POST['telefono'];
-$password = $_POST['password'];
+try {
 
-$passwordHash = password_hash(
-    $password,
-    PASSWORD_DEFAULT
-);
+    $nombre = $_POST['nombre'];
+    $correo = $_POST['correo'];
+    $telefono = $_POST['telefono'];
+    $password = $_POST['password'];
 
-$sql = "SELECT id FROM usuarios WHERE correo = ?";
+    $passwordHash = password_hash($password, PASSWORD_DEFAULT);
+
+    $sql = "SELECT id FROM usuarios WHERE correo = ?";
     $stmt = $conexion->prepare($sql);
     $stmt->execute([$correo]);
 
-    if ($stmt->rowCount() > 0) {
+    if ($stmt->fetch()) {
         echo "error:correo_duplicado";
         exit;
     }
