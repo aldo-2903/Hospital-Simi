577081ec0dc070c5f0b11cd1d8e676473b4881fd
aldo-2903 <eltukis772@gmail.com>
2026-06-16 18:57:21 -0600

    Added conexion.php
    Conexion may be completed

---
+++ conexion.php
@@ -0,0 +1,23 @@
+<?php
+
+$host = "mysql.railway.internal";
+$usuario = "root";
+$password = "IreqgJNcxhUWSJLalBlubYbTbgIBFqbH";
+$basedatos = "hospital";
+$puerto = 3306;
+
+$conexion = new mysqli(
+    $host,
+    $usuario,
+    $password,
+    $basedatos,
+    $puerto
+);
+
+if ($conexion->connect_error) {
+    die("Error de conexión: " . $conexion->connect_error);
+}
+
+echo "Conexión exitosa";
+
+?>
