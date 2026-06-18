f1aa9c2ce5ba014ff1800e460d9a354192fbcf21
aldo-2903 <eltukis772@gmail.com>
2026-06-17 22:24:41 -0600

    Update admin.php

--- admin.php
+++ admin.php
@@ -9,4 +9,28 @@ if(
 }
 
 include("conexion.php");
+
+$sql = "
+SELECT
+c.id_cita,
+c.fecha_cita,
+c.hora_cita,
+c.estado,
+c.tipo_cita,
+c.modalidad,
+c.motivo_consulta,
+p.nombre,
+p.apellido_paterno,
+p.apellido_materno
+FROM cita c
+INNER JOIN paciente p
+ON c.id_paciente = p.id_paciente
+ORDER BY c.fecha_cita DESC, c.hora_cita DESC
+";
+
+$stmt = $conexion->prepare($sql);
+$stmt->execute();
+
+$citas = $stmt->fetchAll(PDO::FETCH_ASSOC);
+
 ?>
